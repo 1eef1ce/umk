d11cac5769ddf6028eb3061133cc7883da6222e9
MironovS <stasmironovweb@gmail.com>
2021-05-31 08:16:21 +0300

    fix svg

--- gulp/tasks/svgSprite.js
+++ gulp/tasks/svgSprite.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
-const svgstore = require('gulp-svgstore')
-const rename = require('gulp-rename')
+const svgstore = require('gulp-svgstore');
+const svgSprites = require('gulp-svg-sprite');
+const rename = require('gulp-rename');
 const svgmin = require('gulp-svgmin'),
   cheerio = require('gulp-cheerio'),
   replace = require('gulp-replace');
@@ -9,6 +10,29 @@ const svgmin = require('gulp-svgmin'),
 
 module.exports = function svgSprite() {
   return gulp.src('src/sprite/*.svg')
+    // .pipe(svgmin({
+    //   js2svg: {
+    //     pretty: true
+    //   }
+    // }))
+    // // remove all fill, style and stroke declarations in out shapes
+    // .pipe(cheerio({
+    //   run: function ($) {
+    //     $('[fill]').removeAttr('fill');
+    //     $('[stroke]').removeAttr('stroke');
+    //     $('[style]').removeAttr('style');
+    //   },
+    //   parserOptions: { xmlMode: true }
+    // }))
+    // // cheerio plugin create unnecessary string '&gt;', so replace it.
+    // .pipe(replace('&gt;', '>'))
+    // // // build svg sprite
+    // .pipe(svgstore({
+    //   inlineSvg: false
+    // }))
+    // .pipe(rename('sprite.svg'))
+    // .pipe(gulp.dest('./build/img/sprite'));
+
     .pipe(svgmin({
       js2svg: {
         pretty: true
@@ -25,11 +49,13 @@ module.exports = function svgSprite() {
     }))
     // cheerio plugin create unnecessary string '&gt;', so replace it.
     .pipe(replace('&gt;', '>'))
-    // // build svg sprite
-    .pipe(svgstore({
-      inlineSvg: true
+    // build svg sprite
+    .pipe(svgSprites({
+      mode: {
+        symbol: {
+          sprite: "sprite.svg",
+        }
+      }
     }))
-    .pipe(rename('sprite.svg'))
     .pipe(gulp.dest('./build/img/sprite'));
-
-}
+};
